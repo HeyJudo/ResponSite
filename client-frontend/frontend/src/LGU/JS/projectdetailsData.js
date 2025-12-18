236700763a88135885aa09e84efd48dcdf68da67
jinshulife <jinrikisha.omela@gmail.com>
2025-12-18 22:43:36 +0800

    Create projectdetailsData.js

---
+++ client-frontend/frontend/src/LGU/JS/projectdetailsData.js
@@ -0,0 +1,45 @@
+export const projectDetails = {
+  name: "Project name",
+  status: "In Progress",
+  progress: "50%",
+  plannedStart: "09/01/2025",
+  targetEnd: "09/30/2025",
+  completionDate: "09/30/2025",
+
+  type: "Filler",
+  location: "BL 11 Filler Street, Filler, QC",
+  budget: "Filler",
+
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.",
+
+  objectives:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.",
+
+  timeline: [
+    {
+      task: "Planning",
+      plannedStart: "12-01-2025",
+      actualStart: "12-01-2025",
+      targetEnd: "12-01-2025",
+      adjustedEnd: "12-01-2025",
+    },
+    {
+      task: "Building",
+      plannedStart: "12-01-2025",
+      actualStart: "12-01-2025",
+      targetEnd: "12-01-2025",
+      adjustedEnd: "12-01-2025",
+    },
+    {
+      task: "Finalization",
+      plannedStart: "11-29-2025",
+      actualStart: "11-29-2025",
+      targetEnd: "11-29-2025",
+      adjustedEnd: "11-29-2025",
+    },
+  ],
+
+  summaryNotes:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.",
+};
