b3fb0556142d8e8109ce21360d6cac22cf36b2f8
jinshulife <jinrikisha.omela@gmail.com>
2025-12-18 21:10:15 +0800

    Create resourcesData.js

---
+++ client-frontend/frontend/src/LGU/JS/resourcesData.js
@@ -0,0 +1,10 @@
+export const resources = [
+  {
+    name: "Filler name",
+    category: "Food",
+    quantity: 1,
+    unit: "packs",
+    location: "Evacuation Center",
+    status: "Low Stock",
+  },
+];
